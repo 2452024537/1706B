5d78ca63003a73fd716a25f166daf3ea86f23189
jasonandjay <342690199@qq.com>
2019-11-05 12:00:32 +0800

    添加知乎案例

---
+++ zhihu/src/views/main.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div>详情页面</div>
+        <router-view></router-view>
+        <footer></footer>
+    </div>
+</template>
---
+++ zhihu/src/views/main/home/type.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul v-for="(item) in list" :key="item.id">
+            <p>{{item.title}}</p>
+            <p>{{item.description}}</p>
+            <img :src="item.banner" alt="">
+            <p>{{`${item.favor}赞同，${item.comment}评论`}}</p>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return {
+            page: 1,
+            pageSize: 10,
+            list: []
+        }
+    },
+    async created(){
+        let data = await axios.get(`/api/list?type=${this.$route.params['type']}&page=${this.page}&pageSize=10`)
+        console.log('router...', data);
+        this.list = data.data;
+    }
+}
+</script>
